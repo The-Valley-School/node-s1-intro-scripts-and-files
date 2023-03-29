ed485879f5a6c1dd6d486038e0f5b52df8ec89ad
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-29 10:44:04 +0200

    Added node sample

---
+++ sample.js
@@ -0,0 +1,21 @@
+console.log("Hola, esto es una prueba");
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// Manera clásica de pedir datos al usuario node nativo
+// const readLine = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// readLine.question("Cómo te llamas? ", (name) => {
+//   console.log(`Hola ${name}`);
+//   readLine.close();
+// });
+
+// Ejemplo con librería
+const prompt = require("prompt-sync")();
+const name = prompt("Cómo te llamas? ");
+console.log(`Hola ${name}`);
